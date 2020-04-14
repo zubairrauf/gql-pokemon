7f703d7e69da9f92f3cfb5988691918ddbb2b237
Zubair Rauf <zubair@zubairrauf.com>
2020-04-14 14:46:08 +0200

    updated pokemon component and added styles

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,10 +3,24 @@ import React from 'react';
 export const Pokemon = (props) => {
     console.log(props)
     return (
-        <div>
-            <h1>{props.pokemon.name}</h1>
-            <p>{props.pokemon.id}</p>
-            <img src={props.pokemon.image} alt={props.pokemon.name}/>
+        <div className='pokemon'>
+            <div className="pokemon__name">
+                <p>{props.pokemon.name}</p>
+            </div>
+            <div className="pokemon__meta">
+                <span> {props.pokemon.maxHP}</span>
+                <span> {props.pokemon.maxCP}</span>
+            </div>
+            <div className="pokemon__image">
+                <img src={props.pokemon.image} alt={props.pokemon.name}/>
+            </div>
+            <div className="pokemon__attacks">
+                {props.pokemon.attacks.special.slice(0, 3).map(attack => {
+                    return (
+                        <span key={`${attack.name}-${attack.damage}`}>{attack.name}</span>
+                    )
+                })}
+            </div>
         </div>
     )
 }
